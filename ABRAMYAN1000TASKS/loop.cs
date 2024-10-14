7199b1dccbd9206dc9c859629f1b46d0868e8b52
Amonullo <amonullo436@gmail.com>
2024-10-14 21:47:45 +0300

    git.push

--- ABRAMYAN1000TASKS/loop.cs
+++ ABRAMYAN1000TASKS/loop.cs
@@ -355,7 +355,8 @@ public class loop
         {
             for (int j = 1; j <= (i - a + 1); j++) 
             {
-                Console.WriteLine(i);    
+                Console.WriteLine(i); 
+                
             }
         }
     }
