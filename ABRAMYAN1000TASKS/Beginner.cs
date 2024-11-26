c1a1823fe3b8d84df0e1457863315cdbd0bda062
Amonullo <amonullo436@gmail.com>
2024-11-26 14:15:48 +0200

    sum_changes_in_begin

--- ABRAMYAN1000TASKS/Beginner.cs
+++ ABRAMYAN1000TASKS/Beginner.cs
@@ -5,9 +5,9 @@ public class Beginner
     //Given the side a of a square, find the perimeter P of the square: P = 4·a.
     public static void B1()
     {
-        var a = double.Parse(Console.ReadLine());
-        
-        Console.WriteLine(a * 4);
+        var a = int.Parse(Console.ReadLine());
+        int b = a * 4;
+        Console.WriteLine(b);
     }
     // Given the side a of a square, find the area S of the square: S = a2
     public static void B2()
@@ -21,9 +21,12 @@ public class Beginner
     {
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
-        Console.WriteLine("Area = " + a * b);
+        double c = a * b;
+        Console.WriteLine("Area = " + c);
+
+        double d = 2 * (a + b);
         
-        Console.WriteLine("Perimetr = "+(a + b)*2);
+        Console.WriteLine("Perimetr = "+d);
     }
         //Given the diameter d of a circle, find the length L of the circle: L = π·d. Use
         // 3.14 for a value of π.
@@ -31,7 +34,8 @@ public class Beginner
     {
         var d = double.Parse(Console.ReadLine());
         float p = 3.14F;
-        Console.WriteLine("Length = " + p * d);
+        double s = p * d;
+        Console.WriteLine("Length = " + s);
         
     }
     //Given the edge a of a cube, find the volume V = a
@@ -41,9 +45,10 @@ public class Beginner
     public static void B5()
     {
         var a = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Volume = " + a * a * a);
-        Console.WriteLine("Area = " + 6 * a);
+        double b = a * a * a;
+        Console.WriteLine("Volume = " + b);
+        double c = 6 * a;
+        Console.WriteLine("Area = " + c);
     }
     //The edges a, b, c of a right parallelepiped are given. Find the volume
     // V = a·b·c and the surface area S = 2·(a·b + b·c + a·c) of the right parallelepiped.
@@ -53,9 +58,10 @@ public class Beginner
     var a = double.Parse(Console.ReadLine());    
     var b = double.Parse(Console.ReadLine());
     var c = double.Parse(Console.ReadLine());
-    
-    Console.WriteLine("Volume = " + a * b * c);
-    Console.WriteLine("Area = " + 2 * (a * b + b * c + a * c));
+    double d = a * b * c;
+    Console.WriteLine("Volume = " + d);
+    double e = 2 * (a * b + b * c + a * c);
+    Console.WriteLine("Area = " + e);
     }
     
     //Begin7. Given the radius R of a circle, find the length L of the circumference and the
@@ -67,15 +73,18 @@ public class Beginner
     {
         var r = double.Parse(Console.ReadLine());
         float p = 3.14F;
-        Console.WriteLine("Length = " + 2*p*r);
-        Console.WriteLine("Area = " + (p * (r * r)));
+        double s = 2 * p * r;
+        Console.WriteLine("Length = " + s);
+        double d = p * (r * r);
+        Console.WriteLine("Area = " + d);
     }
     //Begin8. Given two numbers a and b, find their average: (a + b)/2.
     public static void B8()
     {
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
-        Console.WriteLine("Average = " + (a + b) / 2);
+        double c = (a + b) / 2;
+        Console.WriteLine("Average = " + c);
     }
     //Begin9. Given two nonnegative numbers a and b, find their geometrical mean (a
     // square root of their product): (a·b)1/2
@@ -84,7 +93,8 @@ public class Beginner
     {
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
-        Console.WriteLine("Sqrt = " + Math.Sqrt(a * b));
+        double c = Math.Sqrt(a * b);
+        Console.WriteLine("Sqrt = " + c);
     }
     //Begin10. Two nonzero numbers are given. Find the sum, the difference, the product,
     // and the quotient of their squares.
@@ -92,10 +102,14 @@ public class Beginner
     {
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
-        Console.WriteLine((a*a + b*b));
-        Console.WriteLine((a*a - b*b));
-        Console.WriteLine((a*a * b*b));
-        Console.WriteLine((a*a / b*b));
+        double c = a * a + b * b;
+        Console.WriteLine(c);
+        double d = a * a - b * b;
+        Console.WriteLine(d);
+        double e = a * a * b * b;
+        Console.WriteLine(e);
+        double f = a * a / b * b;
+        Console.WriteLine(f);
     }
     //Begin12. The legs a and b of a right triangle are given. Find the hypotenuse c and the
     // perimeter P of the triangle:
@@ -138,10 +152,12 @@ public class Beginner
     // of π.
     public static void B14()
     {
-        var r = double.Parse(Console.ReadLine());
+        var l = double.Parse(Console.ReadLine());
         double p = 3.14;
-        Console.WriteLine(2 * p * r);
-        Console.WriteLine(p * r * r);
+        double r = l / (2 * p);
+        Console.WriteLine(r);
+        double s = p * r * r;
+        Console.WriteLine(s);
     }
     
     //Begin15. Given the area S of a circle, find the diameter D and the length L of the
@@ -164,8 +180,8 @@ public class Beginner
     {
         var x1 = double.Parse(Console.ReadLine());
         var x2 = double.Parse(Console.ReadLine());
-        
-        Console.WriteLine(Math.Abs(x2 - x1));
+        double x3 = Math.Abs(x2 - x1);
+        Console.WriteLine(x3);
     }
     //Begin17. Three points A, B, C are given on the real axis. Find the length of AC, the
     // length of BC, and the sum of these lengths.
@@ -174,8 +190,8 @@ public class Beginner
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
         var c = double.Parse(Console.ReadLine());
-        double d = Math.Abs(c - a);
-        double e = Math.Abs(c - b);
+        double d = Math.Abs(a - c);
+        double e = Math.Abs(b - c);
         double f = Math.Abs(d + e);
         Console.WriteLine(d);
         Console.WriteLine(e);
@@ -189,11 +205,12 @@ public class Beginner
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
         var c = double.Parse(Console.ReadLine());
-        double d = (Math.Abs(c - a));
-        double e = (Math.Abs(b - c));
+        double d = (Math.Abs(a - c));
+        double e = (Math.Abs(c - b));
+        double f = Math.Abs(d * e);
         Console.WriteLine(d);
         Console.WriteLine(e);
-        Console.WriteLine(Math.Abs(d * e));
+        Console.WriteLine(f);
     }
     //Begin19. The coordinates (x1, y1) and (x2, y2) of two opposite vertices of a rectangle
     // are given. Sides of the rectangle are parallel to coordinate axes. Find the
@@ -223,7 +240,7 @@ public class Beginner
         var x2 = double.Parse(Console.ReadLine());
         var y2 = double.Parse(Console.ReadLine());
         
-        double p = Math.Sqrt(Math.Abs((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)));
+        double p = Math.Sqrt(Math.Abs(((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1))));
         Console.WriteLine(p);
     }
     //Begin21. The coordinates (x1, y1), (x2, y2), (x3, y3) of the triangle vertices are given.
@@ -244,24 +261,28 @@ public class Beginner
         double b = Math.Sqrt((x3 - x1)*(x3 - x1) + (y3 - y1)*(y3 - y1));
         double c = Math.Sqrt((x3 - x2)*(x3 - x2) + (y3 - y2)*(y3 - y2));
         double p = a+b+c;
-        double HalfPer = (a+b+c)/2;
-        double S = Math.Sqrt(HalfPer*(HalfPer - a)*(HalfPer - b)*(HalfPer - c));
+        double halfPer = (a+b+c)/2;
+        double s = Math.Sqrt(halfPer*(halfPer - a)*(halfPer - b)*(halfPer - c));
         
         Console.WriteLine(p);
-        Console.WriteLine(S);
+        Console.WriteLine(s);
     }
-
+//Begin22. Exchange the values of two given variables A and B. Output the new
+// values of A and B.
     public static void B22()
     {
         var a = double.Parse(Console.ReadLine());
         var b = double.Parse(Console.ReadLine());
-
-        (a, b) = (b, a);
+        double c = a; 
+        a = b;
+        b = c;
         Console.WriteLine(a);
         Console.WriteLine(b);
 
     }
-
+//Begin23. Variables A, B, C are given. Change values of the variables by moving the
+// given value of A into the variable B, the given value of B into the variable C,
+// and the given value of C into the variable A. Output the new values of A, B, C.
     public static void B23()
     {
         var a = double.Parse(Console.ReadLine());
@@ -277,7 +298,9 @@ public class Beginner
         Console.WriteLine(b);
         Console.WriteLine(c);
     }
-
+//Begin24. Variables A, B, C are given. Change values of the variables by moving the
+// given value of A into the variable C, the given value of C into the variable B,
+// and the given value of B into the variable A. Output the new values of A, B, C.
     public static void B24()
     {
         var a = double.Parse(Console.ReadLine());
@@ -293,7 +316,12 @@ public class Beginner
         Console.WriteLine(b);
         Console.WriteLine(c);
     }
-
+//Begin25. Given an independent variable x, find the value of a function
+// y = 3x
+// 6
+// 2
+// − 6x
+// − 7.
     public static void B25()
     {
         var x = double.Parse(Console.ReadLine());
@@ -301,7 +329,8 @@ public class Beginner
         double y = (3 * Math.Pow(x, 6) - 6* Math.Pow(x, 2) - 7);
         Console.WriteLine(y);
     }
-
+//Begin26. Given an independent variable x, find the value of a function
+// y = 4(x−3)6 − 7(x−3)3 + 2.
     public static void B26()
     {
         var x = double.Parse(Console.ReadLine());
@@ -309,35 +338,44 @@ public class Beginner
         double y = 4 * Math.Pow(x - 3, 6) - 7 * Math.Pow(x - 3, 3) + 2;
         Console.WriteLine(y);
     }
-
+//Begin27. Given a number A, compute a power A8 using three multiplying operators
+// for computing A2
+// , A4
+// , A8 sequentially. Output all obtained powers of the
+// number A.
     public static void B27()
     {
         var a = double.Parse(Console.ReadLine());
-        double b, c, d;
-        b = a * a;
-        c = a * Math.Pow(a, 3);
-        d = a * Math.Pow(a, 7);
+        var b = a * a;
+        var c = b * b;
+        var d = c * c;
         Console.WriteLine(b);
         Console.WriteLine(c);
         Console.WriteLine(d);
     }
-
+//Begin28. Given a number A, compute a power A15 using five multiplying operators
+// for computing A2
+// , A3
+// , A5
+// , A10
+// , A15 sequentially. Output all obtained powers of
+// the number A.
     public static void B28()
     {
         var a = double.Parse(Console.ReadLine());
-        double b, c, d, e, f;
-            b = a * a;
-        c = a * a * a;
-        d = Math.Pow(a, 5);
-        e = Math.Pow(a, 10);
-        f = Math.Pow(a, 15);
+        var b = a * a;
+        var c = a * b;
+        var d = c * b;
+        var e = d * d;
+        var f = e * d;
         Console.WriteLine(b);
         Console.WriteLine(c);
         Console.WriteLine(d);
         Console.WriteLine(e);
         Console.WriteLine(f);
     }
-
+//Begin29. The angle value α in degrees (0 ≤ α < 360) is given. Convert this value into
+// radians. Take into account that 180° = π radians. Use 3.14 for a value of π.
     public static void B29()
     {
         var a = double.Parse(Console.ReadLine());
@@ -345,7 +383,8 @@ public class Beginner
         double r = a * (p / 180);
         Console.WriteLine(r);
     }
-
+//Begin30. The angle value α in radians (0 ≤ α < 2·π) is given. Convert this value into
+// degrees. Take into account that 180° = π radians. Use 3.14 for a value of π.
     public static void B30()
     {
         var a = double.Parse(Console.ReadLine());
@@ -353,33 +392,43 @@ public class Beginner
         double r = a * (180 / p);
         Console.WriteLine(r);
     }
-
+//Begin31. A Fahrenheit temperature T is given. Convert it into a centigrade
+// temperature. The centigrade temperature TC and the Fahrenheit temperature TF
+// are connected as:
+// TC = (TF − 32)·5/9.
     public static void B31()
     {
         var tf = double.Parse(Console.ReadLine());
         double tc = (tf - 32) * 5 / 9;
         Console.WriteLine(tc);
     }
-
+//Begin32. A centigrade temperature T is given. Convert it into a Fahrenheit
+// temperature. The centigrade temperature TC and the Fahrenheit temperature TF
+// are connected as:
+// TC = (TF − 32)·5/9.
     public static void B32()
     {
         var tc = double.Parse(Console.ReadLine());
         double tf = (tc * 9/5) + 32;
         Console.WriteLine(tf);
     }
-
+//Begin33. X kg of sweet cost A euro. Find the cost of 1 kg and Y kg of the sweets
+// (positive numbers X, A, Y are given).
     public static void B33()
     {
         var x = double.Parse(Console.ReadLine());
         var a = double.Parse(Console.ReadLine());
-        var Y = double.Parse(Console.ReadLine());
+        var z = double.Parse(Console.ReadLine());
         
         double y = a / x;
-        double t = Y * y;
+        double t = z * y;
         Console.WriteLine(y);
         Console.WriteLine(t);
     }
-
+//Begin34. X kg of chocolates cost A euro and Y kg of sugar candies cost B euro
+// (positive numbers X, A, Y, B are given). Find the cost of 1 kg of the chocolates
+// and the cost of 1 kg of the sugar candies. Also determine how many times the
+// chocolates are more expensive than the sugar candies.
     public static void B34()
     {
         var x = double.Parse(Console.ReadLine());
@@ -390,11 +439,14 @@ public class Beginner
         double c = a / x;
         double d = b / y;
         double f = c / d;
-        Console.WriteLine(f);
-        Console.WriteLine(d);
         Console.WriteLine(c);
+        Console.WriteLine(d);
+        Console.WriteLine(f);
     }
-
+//Begin35. A boat velocity in still water is V km/h, river flow velocity is U km/h
+// (U < V). The boat goes along the lake during T1 h and then goes against stream
+// of the river during T2 h. Positive numbers V, U, T1, T2 are given. Find the
+// distance S covered by the boat (distance = time · velocity).
     public static void B35()
     {
         var v = double.Parse(Console.ReadLine());
@@ -404,7 +456,11 @@ public class Beginner
         double s = t1 * (v + u) + t2*(v - u);
         Console.WriteLine(s);
     }
-
+//Begin36. The velocity of the first car is V1 km/h, the velocity of the second car is
+// V2 km/h, the initial distance between the cars is S km. Find the distance
+// between the cars after T hours provided that the distance is increasing. The
+// required distance is equal to a sum of the initial distance and the total distance
+// covered by the both cars (total distance = time · total velocity).
     public static void B36()
     {
         var v1 = double.Parse(Console.ReadLine());
@@ -413,7 +469,11 @@ public class Beginner
         double s = t * v1 + t * v2;
         Console.WriteLine(s);
     }
-
+//Begin37. The velocity of the first car is V1 km/h, the velocity of the second car is
+// V2 km/h, the initial distance between the cars is S km. Find the distance
+// between the cars after T hours provided that at the start time the distance is
+// decreasing. This distance is equal to an absolute value of a difference between
+// the initial distance and the total distance covered by the both cars.
     public static void B37()
     {
         var v1 = double.Parse(Console.ReadLine());
@@ -423,7 +483,8 @@ public class Beginner
         double a = Math.Abs(s - t * (v1 + v2));
         Console.WriteLine(a);
     }
-
+//Begin38. Solve a linear equation A·x + B = 0 with given coefficients A and B (A is
+// not equal to 0).
     public static void B38()
     {
         var a = double.Parse(Console.ReadLine());
@@ -431,7 +492,14 @@ public class Beginner
         double x = -b / a;
         Console.WriteLine(x);
     }
-
+//Begin39. Solve a quadratic equation A·x
+// 2 + B·x + C = 0 with given coefficients A, B,
+// C (A and the discriminant of the equation are positive). Output the smaller
+// equation root and then the larger one. Roots of the quadratic equation may be
+// found by formula
+// x1, 2 = (−B ± (D)1/2)/(2·A),
+// where D = B2
+// − 4·A·C is a discriminant.
     public static void B39()
     {
         var a = double.Parse(Console.ReadLine());
@@ -445,7 +513,13 @@ public class Beginner
         Console.WriteLine(x1);
         Console.WriteLine(x2);
     }
-
+//Begin40. Solve a system of linear equations
+// A1·x + B1·y = C1,
+// A2·x + B2·y = C2
+// with given coefficients A1, B1, C1, A2, B2, C2 provided that the system has the
+// only solution. Use the following formulas:
+// x = (C1·B2 − C2·B1)/D, y = (A1·C2 − A2·C1)/D,
+// where D = A1·B2 − A2·B1.
     public static void B40()
     {
         var a1 = double.Parse(Console.ReadLine());
@@ -458,8 +532,8 @@ public class Beginner
         double d = a1*b2 - a2*b1;
         double x = (c1*b2 - c2*b1)/d;
         double y = (a1*c2 - a2*c1)/d;
-        Console.WriteLine(y);
-        Console.WriteLine(x); 
+        Console.WriteLine(x);
+        Console.WriteLine(y); 
     }
 
 }
